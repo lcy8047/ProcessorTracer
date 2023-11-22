ab0a31ae7a352b0fccd346b8cb381dc7eb0886de
lcy8047 <lcy8047@gmail.com>
2023-11-23 01:19:23 +0900

    Add tracer and trace test

---
+++ trace_test.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "Tracer.h"
+
+int main( int argc, char *argv[] )
+{
+    if ( argc < 2 ) {
+        std::cout << "usage : " << argv[0] << " command" << std::endl;
+        return -1;
+    }
+    char **arg = &argv[1];
+    pid_t pid;
+    pid = fork();
+    if ( pid < 0 ) {
+        std::cerr << "failed to fork" << std::endl;
+        return -1;
+    }
+
+    if ( pid != 0 ) { // parent
+        Tracer *pt = new Tracer( pid );
+		pt->StartTrace();
+        std::cout << "pid : " << pid << std::endl;
+        int status;
+        waitpid( pid, &status, 0 );
+        std::cout << "trace is done" << std::endl;
+        pt->StopTrace();
+    }
+    else {
+        //pause();
+        sleep(1);
+        std::cout << "trace start" << std::endl;
+        execvp( arg[0], (char**)arg );
+    }
+	return 0;
+}
